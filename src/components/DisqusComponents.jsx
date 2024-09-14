ea930f70595c0c939abea1c82610015e6e98c7b5
kuldeeepy <kuldeeep.yadav1@gmail.com>
2024-09-14 21:06:59 +0530

    minor changes

--- src/components/DisqusComponents.jsx
+++ src/components/DisqusComponents.jsx
@@ -1,35 +1,32 @@
-// components/DisqusComments.js
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import { DiscussionEmbed } from "disqus-react";
 
 const DisqusComments = ({ postUrl, identifier, title }) => {
   useEffect(() => {
-    const loadDisqus = () => {
-      const script = document.createElement('script');
-      script.src = 'https://beyondbytes-1.disqus.com/embed.js';
-      script.setAttribute('data-timestamp', +new Date());
-      (document.head || document.body).appendChild(script);
-
-      // Clean up on component unmount
-      return () => {
-        if (document.head.contains(script)) {
-          document.head.removeChild(script);
-        }
-      };
-    };
-
-    if (window.DISQUS === undefined) {
-      loadDisqus();
-    } else {
-      window.disqus_config = function () {
-        this.page.url = postUrl;
-        this.page.identifier = identifier;
-        this.page.title = title;
-      };
-      window.DISQUS.reset({ reload: true });
+    if (window.DISQUS) {
+      window.DISQUS.reset({
+        reload: true,
+        config: function () {
+          this.page.url = postUrl;
+          this.page.identifier = identifier;
+          this.page.title = title;
+        },
+      });
     }
   }, [postUrl, identifier, title]);
 
-  return <div id="disqus_thread"></div>;
+  return (
+    <div id="disqus_thread">
+      <DiscussionEmbed
+        shortname='beyondbytes-1'
+        config={{
+          url: postUrl,
+          identifier: identifier,
+          title: title,
+        }}
+      />
+    </div>
+  );
 };
 
 export default DisqusComments;
